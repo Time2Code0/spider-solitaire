6d2b41b28bba9d3b4b372f224fd42341a81952fd
Time2Code0 <tobiaskann+github@protonmail.com>
2026-04-23 12:32:04 +0200

    feat: add AsciiSignature

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist } from "next/font/google";
 import "./globals.css";
+import { AsciiSignature } from "./layout.client";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -20,6 +21,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
     <html className={geistSans.variable} lang="en">
       <body className="relative font-sans text-base antialiased">
         <div className="isolate flex min-h-screen flex-col">{children}</div>
+        <AsciiSignature />
       </body>
     </html>
   );
